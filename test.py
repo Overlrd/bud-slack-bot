ab37bc1360e1bf18475304379a88f124228cbe3d
Augustin <premieraugust28@gmail.com>
2024-12-16 16:35:22 +0000

    added simple logging to test.py

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-import logging
-
-
 import os
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 
-logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 slack_token = os.getenv("BOT_USER_OAUTH_TOKEN")
 channel_id = os.getenv("CHANNEL_ID")
@@ -15,7 +11,7 @@ client = WebClient(token=slack_token)
 
 if not slack_token:
     raise Exception(
-        "    BOT_USER_OAUTH_TOKEN environement not found, please make sure to provide one     "
+        "BOT_USER_OAUTH_TOKEN environement not found, please make sure to provide one"
     )
 elif not channel_id:
     raise Exception(
@@ -24,8 +20,10 @@ elif not channel_id:
 
 try:
     response = client.chat_postMessage(
-        channel="C085AH5KP9R", text="Hello from your app! :tada:"
+        channel=channel_id, text="Hello from your app! :tada:"
     )
+    print("It's looks like working, check the channel to be sure")
 except SlackApiError as e:
     # You will get a SlackApiError if "ok" is False
     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+    print(f"ERROR: {e.response["error"]}")
